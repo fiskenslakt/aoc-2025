82586f130e012254c0dcb72a76887ed828a5e1a9
Derek <sjukledighet@gmail.com>
2025-12-02 01:25:59 -0500

    Solution for day 2

--- p02.py
+++ p02.py
@@ -1,39 +1,31 @@
 from itertools import batched
 
-from aocd import data, submit
-
-# data = """11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
-# 1698522-1698528,446443-446449,38593856-38593862,565653-565659,
-# 824824821-824824827,2121212118-2121212124"""
+from aocd import data
 
 id_ranges = data.split(",")
 
-id_sum = 0
-# import pudb;pu.db
-# for id_range in id_ranges:
-#     x, y = id_range.split("-")
-#     for _id in range(int(x), int(y)+1):
-#         _id = str(_id)
-#         if _id[:len(_id)//2] == _id[len(_id)//2:]:
-#             # print(_id)
-#             id_sum += int(_id)
-    # print()
-
-# submit(id_sum)
+invalid_id_sum_1 = 0
+invalid_id_sum_2 = 0
 
 for id_range in id_ranges:
-    x, y = id_range.split("-")
-    for _id in range(int(x), int(y)+1):
-        s_id = str(_id)
-        for chunk_size in range(1, len(s_id)):
-            if len(s_id) % chunk_size != 0:
+    min_range, max_range = map(int, id_range.split("-"))
+    for product_id in range(min_range, max_range + 1):
+        product_id_str = str(product_id)
+        i = len(product_id_str)//2
+
+        if product_id_str[:i] == product_id_str[i:]:
+            invalid_id_sum_1 += product_id
+
+        for chunk_size in range(1, len(product_id_str)):
+            if len(product_id_str) % chunk_size != 0:
                 continue
-            chunks = list(batched(s_id, chunk_size))
+            chunks = list(batched(product_id_str, chunk_size))
             for chunk in chunks[1:]:
                 if chunk != chunks[0]:
                     break
             else:
-                id_sum += _id
+                invalid_id_sum_2 += product_id
                 break
 
-submit(id_sum)
+print("Part 1:", invalid_id_sum_1)
+print("Part 2:", invalid_id_sum_2)

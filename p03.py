3dd5f654324ba7761cdc11918f9804f076d6f558
Derek <sjukledighet@gmail.com>
2025-12-03 04:10:49 -0500

    Solution for day 3

--- p03.py
+++ p03.py
@@ -1,12 +1,21 @@
-from itertools import combinations
-
-from aocd import data, submit
+from aocd import data
 
 
-joltage = 0
+def get_largest_joltage(bank, batteries, spots):
+    if spots == 1:
+        return batteries + max(bank)
+
+    i, next_bat = max(enumerate(bank[: -spots + 1]), key=lambda b: (b[1], -b[0]))
+
+    return get_largest_joltage(bank[i + 1 :], batteries + next_bat, spots - 1)
+
+
+joltage_1 = 0
+joltage_2 = 0
 
 for bank in data.splitlines():
-    bat = max(combinations(map(int, bank), 2))
-    joltage += int("".join(map(str, bat)))
+    joltage_1 += int(get_largest_joltage(bank, "", 2))
+    joltage_2 += int(get_largest_joltage(bank, "", 12))
 
-print("Part 1:", joltage)
+print("Part 1:", joltage_1)
+print("Part 2:", joltage_2)

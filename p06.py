814e8ecd84ea84c4fa12c23d50efc9f4c0ce2514
Derek <sjukledighet@gmail.com>
2025-12-06 02:27:05 -0500

    Solution for Day 6

--- p06.py
+++ p06.py
@@ -1,34 +1,27 @@
-from operator import mul, add
 from functools import reduce
+from operator import add, mul
 
-from aocd import data, submit
+from aocd import data
 
-# data = """123 328  51 64 
-#  45 64  387 23 
-#   6 98  215 314
-# *   +   *   +  """
+problem_sheet = [line.split() for line in data.splitlines()]
+problems = list(zip(*problem_sheet))
+grand_total = 0
 
+for problem in problems:
+    *operands, operator = problem
+
+    op = add if operator == "+" else mul
+    grand_total += reduce(op, map(int, operands))
 
 real_problem_sheet = list(zip(*data.splitlines()))[::-1]
-
-# problem_sheet = [line.split(" ") for line in data.splitlines()]
-# problems = list(zip(*problem_sheet))
-# grand_total = 0
-
-# for problem in problems:
-#     *operands, operator = problem
-
-#     op = add if operator == "+" else mul
-#     grand_total += reduce(op, map(int, operands))
-
-# submit(grand_total)
-# import pudb# ;pu.db
-grand_total = 0
+real_grand_total = 0
 i = 0
 while i < len(real_problem_sheet):
     problem = []
 
-    while i < len(real_problem_sheet) and not all(j == " " for j in real_problem_sheet[i]):
+    while i < len(real_problem_sheet) and not all(
+        j == " " for j in real_problem_sheet[i]
+    ):
         problem.append(real_problem_sheet[i])
         i += 1
     operator = problem[-1][-1]
@@ -38,8 +31,8 @@ while i < len(real_problem_sheet):
     for operand in problem:
         operands.append(int("".join(operand[:-1])))
 
-    grand_total += reduce(op, operands)
-
+    real_grand_total += reduce(op, operands)
     i += 1
 
-submit(grand_total)
+print("Part 1:", grand_total)
+print("Part 2:", real_grand_total)

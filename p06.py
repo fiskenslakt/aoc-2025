2a383832a71dea7639e03f20e034582dbad04eeb
Derek <sjukledighet@gmail.com>
2025-12-06 01:59:05 -0500

    Day 6 part 2

--- p06.py
+++ p06.py
@@ -8,14 +8,38 @@ from aocd import data, submit
 #   6 98  215 314
 # *   +   *   +  """
 
-problem_sheet = [line.split() for line in data.splitlines()]
-problems = list(zip(*problem_sheet))
+
+real_problem_sheet = list(zip(*data.splitlines()))[::-1]
+
+# problem_sheet = [line.split(" ") for line in data.splitlines()]
+# problems = list(zip(*problem_sheet))
+# grand_total = 0
+
+# for problem in problems:
+#     *operands, operator = problem
+
+#     op = add if operator == "+" else mul
+#     grand_total += reduce(op, map(int, operands))
+
+# submit(grand_total)
+# import pudb# ;pu.db
 grand_total = 0
+i = 0
+while i < len(real_problem_sheet):
+    problem = []
 
-for problem in problems:
-    *operands, operator = problem
-
+    while i < len(real_problem_sheet) and not all(j == " " for j in real_problem_sheet[i]):
+        problem.append(real_problem_sheet[i])
+        i += 1
+    operator = problem[-1][-1]
     op = add if operator == "+" else mul
-    grand_total += reduce(op, map(int, operands))
+
+    operands = []
+    for operand in problem:
+        operands.append(int("".join(operand[:-1])))
+
+    grand_total += reduce(op, operands)
+
+    i += 1
 
 submit(grand_total)

0ed9d4b94526bd85ba89fa297c7c62d9c8740b2c
Derek <sjukledighet@gmail.com>
2025-12-11 02:35:06 -0500

    Day 11 part 2

--- p11.py
+++ p11.py
@@ -1,4 +1,5 @@
 from collections import deque
+from functools import cache
 
 from aocd import data, submit
 
@@ -14,20 +15,41 @@ from aocd import data, submit
 # iii: out
 # """
 
-data = """svr: aaa bbb
-aaa: fft
-fft: ccc
-bbb: tty
-tty: ccc
-ccc: ddd eee
-ddd: hub
-hub: fff
-eee: dac
-dac: fff
-fff: ggg hhh
-ggg: out
-hhh: out
-"""
+# data = """svr: aaa bbb
+# aaa: fft
+# fft: ccc
+# bbb: tty
+# tty: ccc
+# ccc: ddd eee
+# ddd: hub
+# hub: fff
+# eee: dac
+# dac: fff
+# fff: ggg hhh
+# ggg: out
+# hhh: out
+# """
+
+
+@cache
+def dfs(device, visit_dac, visit_fft):
+    if device == "out" and visit_dac and visit_fft:
+        return 1
+    elif device == "out":
+        return 0
+
+    paths = []
+
+    for output in graph[device]:
+        if output == "dac":
+            paths.append(dfs(output, True, visit_fft))
+        elif output == "fft":
+            paths.append(dfs(output, visit_dac, True))
+
+        paths.append(dfs(output, visit_dac, visit_fft))
+
+    return sum(paths)
+
 
 graph = {}
 
@@ -36,17 +58,43 @@ for line in data.splitlines():
 
     graph[device] = [output for output in outputs.split()]
 
-queue = deque(["you"])
-paths = 0
+# queue = deque(["you"])
+# paths = 0
 
-while queue:
-    device = queue.popleft()
+# while queue:
+#     device = queue.popleft()
 
-    if device == "out":
-        paths += 1
-        continue
+#     if device == "out":
+#         paths += 1
+#         continue
 
-    for output in graph[device]:
-        queue.append(output)
+#     for output in graph[device]:
+#         queue.append(output)
 
 # print("Part 1:", paths)
+
+# queue = deque([("svr", False, False)])
+# paths = 0
+# seen = set()
+
+# while queue:
+#     device, visit_dac, visit_fft = queue.popleft()
+
+#     if device == "out" and visit_dac and visit_fft:
+#         paths += 1
+#         continue
+#     elif device == "out":
+#         continue
+
+#     for output in graph[device]:
+#         if output == "dac":
+#             queue.append((output, True, visit_fft))
+#         elif output == "fft":
+#             queue.append((output, visit_dac, True))
+
+#         if output not in seen:
+#             queue.append((output, visit_dac, visit_fft))
+#             seen.add(output)
+
+# print(paths)
+submit(dfs("svr", False, False))
